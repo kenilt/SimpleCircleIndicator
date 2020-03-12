629f256d02caa381191445ea90441f1f578c609b
Thang Nguyen <nhthang.khtn@gmail.com>
2020-03-12 11:25:35 +0700

    Add CirclePageIndicator and sample app

---
+++ app/src/main/java/com/kenilt/simplecircleindicator/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.kenilt.simplecircleindicator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        vpPager.adapter = TextPagerAdapter(this, DataGenerator.generateList())
+        indicator.setViewPager(vpPager)
+    }
+}
