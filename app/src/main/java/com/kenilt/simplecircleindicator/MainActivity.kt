5d10a8d4799401ec450a3172af414b758058ea65
Thang Nguyen <nhthang.khtn@gmail.com>
2024-08-23 09:29:06 +0700

    Upgrade the libraries and gradle to the latest version

--- app/src/main/java/com/kenilt/simplecircleindicator/MainActivity.kt
+++ app/src/main/java/com/kenilt/simplecircleindicator/MainActivity.kt
@@ -2,13 +2,17 @@ package com.kenilt.simplecircleindicator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.viewpager.widget.ViewPager
+import com.kenilt.circleindicator.CirclePageIndicator
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val vpPager = findViewById<ViewPager>(R.id.vpPager)
+        val indicator = findViewById<CirclePageIndicator>(R.id.indicator)
 
         vpPager.adapter = TextPagerAdapter(this, DataGenerator.generateList())
         indicator.setViewPager(vpPager)
